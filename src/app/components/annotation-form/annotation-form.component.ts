08e1148854de71215de368ce04660eff68a755b1
federico masci <federico.masci@coni.it>
2020-12-10 12:12:05 +0100

    - Implementata la modifica di una annotazione

--- src/app/components/annotation-form/annotation-form.component.ts
+++ src/app/components/annotation-form/annotation-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AnnotationBk } from 'src/app/models/annotation-bk';
 import { AuthService } from 'src/app/services/auth.service';
 import { DashboardService } from 'src/app/services/dashboard.service';
 
@@ -13,26 +14,68 @@ export class AnnotationFormComponent implements OnInit {
 
   modelvalue: string;
   modelcontent: string;
+  modelid: number;
 
   constructor(
     private dashboard: DashboardService,
     private auth: AuthService,
+    private active: ActivatedRoute,
     private router: Router
   ) { }
 
-  sendAnnotation(form: NgForm): void {
-    this.dashboard.postAnnotation(form)
+  sendAnnotation(form: NgForm, close: boolean): void {
+    if (form.value.Annotation_iId === 0) {
+      this.dashboard.postAnnotation(form)
+        .subscribe(res => {
+          if (res.status === 'OK') {
+            if (close) {
+              this.router.navigateByUrl('/dashboard/annotazioni');
+            } else {
+              const item: AnnotationBk = res.item as AnnotationBk;
+              this.bind(item);
+            }
+          }
+        });
+    } else {
+      this.dashboard.putAnnotation(form)
+        .subscribe(res => {
+          if (res.status === 'OK') {
+            if (close) {
+              this.router.navigateByUrl('/dashboard/annotazioni');
+            } else {
+              const item: AnnotationBk = res.item as AnnotationBk;
+              this.bind(item);
+            }
+          }
+        });
+    }
+  }
+
+  getAnnotation(id: number): void {
+    this.dashboard.getAnnotation(id)
       .subscribe(res => {
-        if (res.status === 'OK') {
-          this.router.navigateByUrl('/dashboard/annotazioni');
-        }
-       });
+        console.log(res);
+        const item = res.item as AnnotationBk;
+        this.bind(item);
+      });
   }
 
   ngOnInit(): void {
     if (!this.auth.notExpired()) {
       this.router.navigateByUrl('login');
+    } else {
+      const id: number = parseInt(this.active.snapshot.paramMap.get('id'), 10);
+      console.log('id', id);
+      if (id > 0) {
+        this.getAnnotation(id);
+      }
     }
+  }
+
+  private bind(item: AnnotationBk): void {
+    this.modelid = item.Annotation_iId;
+    this.modelvalue = item.Annotation_sValue;
+    this.modelcontent = item.Annotation_sContent;
   }
 
 }
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -13,20 +13,42 @@ const API = environment.url;
 })
 export class DashboardService {
 
-  private options: HttpHeaders = new HttpHeaders().set('content-type', 'application/x-www-form-urlencoded');
-
   constructor(private http: HttpClient) { }
 
   getAnnotations(): Observable<ApiResponse> {
-    return this.http.get<ApiResponse>(`${API}/annotations.php?task=backend`);
+    const token = localStorage.getItem('token');
+    const options = this.headers(token);
+
+    return this.http.get<ApiResponse>(`${API}/annotations.php`, {headers: options});
+  }
+
+  getAnnotation(id: number): Observable<ApiResponse> {
+    const token = localStorage.getItem('token');
+    const options = this.headers(token);
+
+    return this.http.get<ApiResponse>(`${API}/annotations.php?id=${id}`, { headers: options });
   }
 
   postAnnotation(datiForm: NgForm): Observable<ApiResponse> {
     const body = this.body(datiForm);
     const token = localStorage.getItem('token');
-    const options = this.options.append('Authorization', 'Bearer ' + token);
+    const options = this.headers(token);
 
     return this.http.post<ApiResponse>(`${API}/annotations.php`, body, { headers: options })
+      .pipe(
+        map((res: ApiResponse) => {
+          return res;
+        }),
+        catchError(this.errorHandler)
+      );
+  }
+
+  putAnnotation(datiForm: NgForm): Observable<ApiResponse> {
+    const body = this.body(datiForm);
+    const token = localStorage.getItem('token');
+    const options = this.headers(token);
+
+    return this.http.put<ApiResponse>(`${API}/annotations.php`, body, { headers: options })
       .pipe(
         map((res: ApiResponse) => {
           return res;
@@ -54,10 +76,19 @@ export class DashboardService {
 
   private body(df: NgForm): HttpParams {
     const params = new HttpParams()
+      .set('Annotation_iId', df.value.Annotation_iId)
       .set('Annotation_sValue', df.value.Annotation_sValue)
       .set('Annotation_sContent', df.value.Annotation_sContent);
 
     return params;
+  }
+
+  private headers(token: string): HttpHeaders {
+    const headers: HttpHeaders = new HttpHeaders()
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .set('Authorization', 'Bearer ' + token);
+
+    return headers;
   }
 
 }
